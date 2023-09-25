c8bd198fa02db846490814553c068e34f66bff20
Matheus Melo <mtsmelo95@gmail.com>
2023-09-25 16:30:42 -0300

    implementação de novos componentes

--- src/Components/Min_Max.jsx
+++ src/Components/Min_Max.jsx
@@ -1,11 +1,21 @@
-import './Min_Max.css'
+import React, { Fragment } from "react";
+import "./Min_Max.css";
 
-function Min_Max() {
-
+function Min_Max(props) {
   return (
-    <>
-    </>
-  )
+    <Fragment>
+      <div className="min">
+        <p>Temperatura Mínima</p>
+        {props.min}
+        {<span>°</span>}
+      </div>
+      <div className="max">
+        <p>Temperatura Máxima</p>
+        {props.max}
+        {<span>°</span>}
+      </div>
+    </Fragment>
+  );
 }
 
-export default Min_Max
+export default Min_Max;
--- src/Components/TempHora.jsx
+++ src/Components/TempHora.jsx
@@ -1,10 +1,16 @@
+import React from 'react'
 import './TempHora.css'
 
-function TempHora() {
-
+const TempHora = (props) => {
   return (
-    <>
-    </>
+    <div>
+      <p>Temperatura por hora</p>
+      {props.temperature2.map((temp, index) => (
+        <div key={index}>
+          <span>{index}:00</span> {temp}°
+        </div>
+      ))}
+    </div>
   )
 }
 
