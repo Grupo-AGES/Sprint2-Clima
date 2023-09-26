af815f4eecea8b80f4da87f36ab41bb61b115e59
killiandb <killiandomingues57@gmail.com>
2023-09-26 15:41:16 -0300

    temMax e tempMin dos dias da semana e componente DiasDaSemana renderizando

--- src/Components/DiasDaSemana.jsx
+++ src/Components/DiasDaSemana.jsx
@@ -1,11 +1,37 @@
-import './DiasDaSemana.css'
+import './DiasDaSemana.css';
+import React, { Fragment } from "react";
 
-function DiasDaSemana() {
+function DiasDaSemana(props) {
+  const { maxSemana, minSemana } = props;
 
   return (
-    <>
-    </>
+    <Fragment>
+      <div className='tempMinSemana'>
+        <p>Temperatura Mínima</p>
+        {Array.isArray(minSemana) ? (
+          minSemana.map((minTemp, index) => (
+            <div key={index}>
+              {minTemp}°
+            </div>
+          ))
+        ) : (
+          <div>Valores de temperatura mínima não estão disponíveis.</div>
+        )}
+      </div>
+      <div className='tempMaxSemana'>
+        <p>Temperatura Máxima</p>
+        {Array.isArray(maxSemana) ? (
+          maxSemana.map((maxTemp, index) => (
+            <div key={index}>
+              {maxTemp}°
+            </div>
+          ))
+        ) : (
+          <div>Valores de temperatura máxima não estão disponíveis.</div>
+        )}
+      </div>
+    </Fragment>
   )
 }
 
-export default DiasDaSemana
+export default DiasDaSemana;
